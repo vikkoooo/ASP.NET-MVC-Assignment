0c351d6bb22ba2a8672073760100a52807560d30
Viktor Lundberg <vikkolundberg@gmail.com>
2022-03-09 21:02:53 +0100

    json deserialization of comic object
    /comic/debug to check status

---
+++ NoA_API-Assignment/Controllers/ComicController.cs
@@ -0,0 +1,45 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Newtonsoft.Json;
+using NoA_API_Assignment.Models;
+
+namespace NoA_API_Assignment.Controllers
+{
+	public class ComicController : Controller
+	{
+		private HttpClient client; // declare client
+		private string url = "https://xkcd.com/info.0.json"; // our api address
+		
+		// ActionResult needs to be async
+		public async Task<ActionResult> Debug()
+		{
+			InitializeClient();
+			
+			HttpResponseMessage response = await client.GetAsync(url); // try to get a response
+
+			if (response.IsSuccessStatusCode)
+			{
+				var jsonString = await response.Content.ReadAsStringAsync(); // this will be our json string
+				var comic = JsonConvert.DeserializeObject<Comic>(jsonString); // deserialize the string
+				
+				return Content(comic.num.ToString()); // print something to check it was converted correct
+			}
+			else
+			{
+				return Content("no response");
+			}
+		}
+		
+		// Initialize our HTTP client. Maybe there is a better place to do this? Now we kinda initialize every time
+		// we load our page
+		// TODO: optimize the initialization of the client
+		public void InitializeClient()
+		{
+			client = new HttpClient();
+			client.DefaultRequestHeaders.Accept.Clear();
+			client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+		}
+	}
+}
---
+++ NoA_API-Assignment/Models/Comic.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace NoA_API_Assignment.Models
+{
+	[Serializable]
+	public class Comic
+	{
+		// if using this line it will change the name to whatever we write here, instead of our local variable name
+		//[JsonProperty("month_")]
+		public string month { get; set; }
+		public int num { get; set; }
+		public string link { get; set; }
+		public string year { get; set; }
+		public string news { get; set; }
+		public string safe_title { get; set; }
+		public string transcript { get; set; }
+		public string alt { get; set; }
+		public string img { get; set; }
+		public string title { get; set; }
+		public string day { get; set; }
+	}
+}
